4307223a08c0bd2f16f26871e2f0511f5d24ccb8
松井優樹 <matsuiyuuki@matsuiyuukinoMacBook-Pro.local>
2020-07-14 00:24:12 +0900

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,10 +5,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+      post users_path, params: { user: { name:"",
+                                         email:"user@invalid",
+                                         password:"foo",
+                                         password_confirmation:"bar" } }
     end
     assert_template 'users/new'
     #assert_select 'div#<CSS id for error explanation>'
@@ -25,6 +25,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.empty?
+    assert is_logged_in?
   end
 end
