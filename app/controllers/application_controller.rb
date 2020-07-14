cf2948fd714836165a7f6ae3467115dd223e04d2
松井優樹 <matsuiyuuki@matsuiyuukinoMacBook-Pro.local>
2020-07-14 23:54:37 +0900

    Implemenr advanced login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
 end
